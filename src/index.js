7fd162f5afd3cd07903f94c886dbc5ca9f6b9d20
Kurnosov Valentyn <deyke66@gmail.com>
2023-05-01 18:35:55 +0200

    async-function

--- src/index.js
+++ src/index.js
@@ -1,66 +1,131 @@
 import './css/styles.css';
 import Notiflix, { Notify } from 'notiflix';
 import { fetchSearchParameters } from './js/fetchSearchParameters';
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import axios from 'axios';
 
 const axios = require('axios');
 
 const refs = {
     searchForm: document.querySelector('.search-form'),
-    formInput: document.querySelector('[name=searchQuery]'),
+    formBtn: document.querySelector('.form-btn'),
     galleryBox: document.querySelector('.gallery'),
     loadMoreBtn: document.querySelector('.load-more')
 }
-const { searchForm, formInput, galleryBox, loadMoreBtn } = refs;
+const { searchForm, formBtn, galleryBox, loadMoreBtn } = refs;
 
 let pageCount = 1;
+loadMoreBtn.style.display = 'none';
 
 searchForm.addEventListener('submit', onSubmitButtonSearch);
 
-function onSubmitButtonSearch(e) {
-    e.preventDefault();
-    pageCount = 1;
-    galleryBox.innerHTML = '';
-    const { searchQuery } = e.target;
+// function onSubmitButtonSearch(e) {
+//   e.preventDefault();
+//   pageCount = 1;
+//   galleryBox.innerHTML = '';
+  
+//   const { searchQuery } = e.target;
+//   const inputValue = searchQuery.value.trim()
 
-    fetchSearchParameters(searchQuery.value, pageCount).then(resp => {
-        const { data: { total } } = resp
-        if (!total) {
-            return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-        }
-
-        createMarkupForGallery(resp.data.hits);
-        
-        loadMoreBtn.addEventListener('click', () => {
-            pageCount += 1;
-            fetchSearchParameters(searchQuery.value, pageCount).then(response => {
-                createMarkupForGallery(response.data.hits)
-            });
-            
-        })
-        
-    }).catch(err => console.log(err));
+//   fetchSearchParameters(inputValue, pageCount).then(resp => {
+//     const { data: { total, hits, totalHits } } = resp;
     
-}
-console.log(pageCount)
+//     if (!total) {
+//       return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+//     }
+
+//     if (inputValue === '') {
+//       return
+//     }
+
+//     Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+//     loadMoreBtn.style.display = 'block';
+//     createMarkupForGallery(hits);
+        
+//     loadMoreBtn.addEventListener('click', () => {
+//       pageCount += 1;
+//       fetchSearchParameters(inputValue, pageCount).then(response => {
+//         const { data: { hits, totalHits } } = response;
+//         console.log(hits.length)
+//         createMarkupForGallery(hits);
+
+//         if (totalHits === hits.length) {
+//           loadMoreBtn.disabled = true;
+//           Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+//         }
+
+//       });      
+//     })     
+//   }).catch(err => console.log(err));
+
+// }
 
 function createMarkupForGallery(value) {
-    const markup = value.map(({ webformatURL, tags, likes, views, comments, downloads }) => `<div class="photo-card">
+  const markup = value.map(({ webformatURL, tags, likes, views, comments, downloads }) => `<div class="photo-card">
   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes: ${likes}</b>
+      <b>Likes</b>${likes}
     </p>
     <p class="info-item">
-      <b>Views: ${views}</b>
+      <b>Views</b>${views}
     </p>
     <p class="info-item">
-      <b>Comments: ${comments}</b>
+      <b>Comments</b> ${comments}
     </p>
     <p class="info-item">
-      <b>Downloads: ${downloads}</b>
+      <b>Downloads</b>${downloads}
     </p>
   </div>
 </div>`).join('');
-    return galleryBox.insertAdjacentHTML('beforeend', markup);
+  return galleryBox.insertAdjacentHTML('beforeend', markup);
 }
 
+async function onSubmitButtonSearch(e) {
+  e.preventDefault();
+  clearHtml();
+  pageCount = 1;
+  formBtn.disabled = true;
+
+  const { searchQuery } = e.target;
+  const inputValue = searchQuery.value.trim()
+
+  try {
+    const fetchUrl = await fetchSearchParameters(inputValue, pageCount);
+    const { data: { hits, totalHits, total } } = fetchUrl;
+
+    if (!total) {
+      return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    }
+
+    createMarkupForGallery(hits);
+    formBtn.disabled = false;
+    loadMoreBtn.style.display = 'block';
+    
+
+  } catch (error) {
+    console.log(error)
+  }
+
+  loadMoreBtn.addEventListener('click', async () => {
+      loadMoreBtn.disabled = true;
+      pageCount += 1;
+      try {
+        let fetchUrlForBtn = await fetchSearchParameters(inputValue, pageCount);
+        let { data: { hits: hitsForBtn, totalHits, total } } = fetchUrlForBtn;
+
+        createMarkupForGallery(hitsForBtn);  
+        loadMoreBtn.disabled = false;
+
+      } catch (err) {
+        console.log(err)
+      }
+      
+  })
+  
+}
+
+function clearHtml() {
+  galleryBox.innerHTML = '';
+}
--- src/js/fetchSearchParameters.js
+++ src/js/fetchSearchParameters.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-export function fetchSearchParameters(value, page) {
+export async function fetchSearchParameters(value, page) {
     const API_KEY = '35922682-1b431e882647e6bfef105f4f6';
+    const BASE_URL = 'https://pixabay.com/api/';
+    const result = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=4&page=${page}`);
     
-    return axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
+    return result;
 }
